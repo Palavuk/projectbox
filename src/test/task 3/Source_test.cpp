29d5d78bff1a0747f5710b64d8a86ab520cae4bb
Nikita Litvinov <lit.nk.98@gmail.com>
2020-01-18 21:55:14 +0300

    tests

--- src/test/task 3/Source_test.cpp
+++ src/test/task 3/Source_test.cpp
@@ -1,2 +1,61 @@
-//#include <gtest/gtest.h>
+#include <gtest/gtest.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 
+#include <project/"task 3"/Sourse.h>
+
+template <class ... Args>
+std::string resultult(std::string format, Args ... args)
+{
+	std::stringstream stream;
+	message(stream, format, args...);
+	return stream.str();
+};
+
+TEST(message, argument_count)
+{
+	EXPECT_EQ("a + 2 = 3 b c 10", result("% + % = % % % %", 'a', 2, 3.0, 'b', 'c', 10.0));
+	EXPECT_EQ("% + % = %", result("% + % = %"));
+	EXPECT_EQ("a + 2 = 3", result("% + % = %", 'a', 2, 3.0, 1, "abcdef", 156));
+	EXPECT_EQ("% and %", result("%", "% and %", 1, 2));
+	EXPECT_EQ("1 2 3 4 5 6 7 8 9 10", result("% % % % % % % % % %", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+	EXPECT_EQ("1 2 3 4 5 6", result("1 2 3 4 % %", 5, 6));
+	EXPECT_EQ("", result(""));
+	EXPECT_EQ("a 5 a", result("a % a", 5));
+}
+
+TEST(cat, correctness)
+{
+	std::array<int, 3> t1_i3_0{ 1, 2, 3 }, t1_i3_1{ 4, 5, 6 };
+	std::array<int, 6> e1_i6{ 1, 2 ,3 ,4 ,5 ,6 };
+	EXPECT_EQ(e1_i6, cat(t1_i3_0, t1_i3_1));
+
+	std::array<int, 2> t2_i2_0{ 1, 2 }, t2_i2_1{ 3, 4 }, t2_i2_2{ 5, 6 };
+	std::array<int, 6> e2_i6{ 1, 2, 3, 4, 5, 6 };
+	EXPECT_EQ(e2_i6, cat(t2_i2_0, t2_i2_1, t2_i2_2));
+
+	std::array<int, 1> t3_i1_0{ 1 },
+		t3_i1_1{ 2 },
+		t3_i1_2{ 3 },
+		t3_i1_3{ 4 },
+		t3_i1_4{ 5 },
+		t3_i1_5{ 6 };
+	std::array<int, 6> e3_i6{ 1, 2, 3, 4, 5, 6 };
+	EXPECT_EQ(e3_i6, cat(t3_i1_0, t3_i1_1, t3_i1_2, t3_i1_3, t3_i1_4, t3_i1_5));
+}
+
+TEST(uncat, correctness)
+{
+	std::array<int, 3> t1_i3_0{ 1, 2, 3 }, t1_i3_1{ 4, 5, 6 },
+		e1_i3_0{ 11, 22, 33 }, e1_i3_1{ 44, 55, 66 };
+	std::array<int, 6> t1_i6{ 11, 22 ,33 ,44 ,55 ,66 };
+	uncat(t1_i3_0, t1_i3_1) = t1_i6;
+	EXPECT_TRUE(t1_i3_0 == e1_i3_0 && t1_i3_1 == e1_i3_1);
+
+
+	EXPECT_NO_THROW({ uncat(t1_i3_0, t1_i3_1); });
+
+	EXPECT_NO_THROW({ uncat(t1_i3_0, t1_i3_1, t1_i3_0, t1_i3_1); });
+}
