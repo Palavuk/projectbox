466f1d8e7c2f92b8c9f5c1951aae2be12f06b7d6
Nikita Litvinov <lit.nk.98@gmail.com>
2020-01-19 16:31:11 +0300

    rotation change

--- src/project/task_10/vec_rotation.h
+++ src/project/task_10/vec_rotation.h
@@ -4,9 +4,9 @@ bool rotation(int angle, std::array<float, 2>& vector){
     if (angle % 90 == 0){
         int rot = (angle / 90) % 4;
         if (rot == 2 || rot == 3)
-            vector[0] *= -1;
-        if (rot == 1 || rot == 2 )
             vector[1] *= -1;
+        if (rot == 1 || rot == 2 )
+            vector[0] *= -1;
         return 1;
     }
     else
