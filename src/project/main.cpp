fe0be042fe6ed77719603a526164f6243c110258
Nikita Litvinov <lit.nk.98@gmail.com>
2020-01-22 23:01:17 +0300

    initial commit

--- src/project/main.cpp
+++ src/project/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "task_5/merge_sort.h"
+
 
 int main() {
+	std::vector<int> vec;
+	for (int i = 10; i > 1; i--)
+		vec.push_back(i);
+	
+	std::cout<<*vec.begin()<<" "<<*vec.end()<<std::endl;
+    merge_sort(vec.begin(), vec.end());
+	std::cout<<*vec.begin()<<" "<<*vec.end()<<std::endl;
+
 	return 0;
 }
