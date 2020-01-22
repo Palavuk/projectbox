e15654c05dba598d0b97890cc3cabb178a5ab889
Nikita Litvinov <lit.nk.98@gmail.com>
2020-01-22 14:42:15 +0300

    initial commit

--- src/test/task_4/where_test.cpp
+++ src/test/task_4/where_test.cpp
@@ -4,10 +4,10 @@
 
 struct where_test_param
 {
-	Matrix<bool> bool_mask;
-	Matrix<float> input_true;
-	Matrix<float> input_false;
-	Matrix<float> output;
+	matrix<bool> bool_mask;
+	matrix<float> input_true;
+	matrix<float> input_false;
+	matrix<float> output;
 };
 
 class where_test : public ::testing::TestWithParam<where_test_param> {};
@@ -16,7 +16,7 @@ TEST_P(where_test, _)
 {
 	const where_test_param& param = GetParam();
 	
-	Matrix<float> output = where(param.bool_mask, param.input_true, param.input_false);
+	matrix<float> output = where(param.bool_mask, param.input_true, param.input_false);
 
 	EXPECT_EQ(param.output, output);
 		
@@ -27,10 +27,10 @@ INSTANTIATE_TEST_CASE_P(
 	where_test,
 	::testing::Values(
 		where_test_param{
-			Matrix<bool>(std::vector<bool>({1, 0, 0, 0, 0, 0}), 2, 3),
-			Matrix<float>(std::vector<float>({1, 2, 3, 4, 5, 6}), 2, 3),
-			Matrix<float>(std::vector<float>({11, 22, 33, 44, 55, 66}), 2, 3),
-			Matrix<float>(std::vector<float>({1, 22, 33, 44, 55, 66}), 2, 3)
+			matrix<bool>(std::vector<bool>({1, 0, 0, 0, 0, 0}), 2, 3),
+			matrix<float>(std::vector<float>({1, 2, 3, 4, 5, 6}), 2, 3),
+			matrix<float>(std::vector<float>({11, 22, 33, 44, 55, 66}), 2, 3),
+			matrix<float>(std::vector<float>({1, 22, 33, 44, 55, 66}), 2, 3)
 		}
 	)
 );
